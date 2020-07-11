511c5c9e1f004c959a0767aa173bb101b276bfad
yurim <sur801@naver.com>
2020-07-11 13:17:52 +0900

    Revert "Merge branch 'master' of https://github.com/woojune511/CS496"
    This reverts commit 7fa823415995c7503f09e3064c7b46dab5cbeff6, reversing
    changes made to 2f79557548d0c763730949072b2150f8637ebc1c.

--- app/src/main/java/com/example/cs496_tab_tutorial/ThirdFragment.java
+++ app/src/main/java/com/example/cs496_tab_tutorial/ThirdFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class ThirdFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
@@ -14,23 +13,6 @@ public class ThirdFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup containter, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_fragment3, null);
-
-        Button OnOffButton = (Button) view.findViewById(R.id.OnOffButton);
-        Button NotifButton = (Button) view.findViewById(R.id.NotifButton);
-
-        OnOffButton.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View view){
-
-            }
-        });
-
-        NotifButton.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View view){
-
-            }
-        });
 
         return view;
     }
